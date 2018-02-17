a27568fbbb8a7a9c610617a123c43c30d71e1788
Renan Farias <renan-ej@hotmail.com.br>
2018-02-17 14:05:04 +0000

    A6020: Use prebuilt libboringssl

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,10 +1,10 @@
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
-    libboringssl-compat \
-    libbson \
     libshim_gui \
     Snap
+
+#    libboringssl-compat \
 
 # Stlport
 PRODUCT_PACKAGES += \
