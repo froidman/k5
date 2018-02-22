fac0e6347f82b460b8c999473d588162f4e6b36a
Renan Farias <renan-ej@hotmail.com.br>
2018-02-22 04:49:40 +0000

    A6020: Add dalvik heap properties

--- product/dalvik.mk
+++ product/dalvik.mk
@@ -1,2 +1,3 @@
 # HWUI memory limits
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
